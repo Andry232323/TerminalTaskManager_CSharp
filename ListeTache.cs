6d69779e3b0bbb3ac3a4245e869c3b5337d7e8a5
Andry <aandrytsilavina@gmail.com>
2023-06-08 12:26:15 +0200

    Task management

--- ListeTache.cs
+++ ListeTache.cs
@@ -47,13 +47,53 @@ public class ListeTask {
     }
     
     public void addTask(Task t) {
-        tasks.Add(t);
+        this.tasks.Add(t);
         using (StreamWriter sw = new StreamWriter(ListTaskDir + "\\" + this.name + ".txt" , true))
         {
             sw.WriteLine("#~~~" + t.name + "~~~" + t.id + "~~~" + t.dCreat + "~~~" + t.dLastModif + "~~~" + t.descr); 
         }
         this.dtLastModif = DateTime.Now;
+        Console.WriteLine("La tache " + t.name + " a été ajouté à " + this.name);
     }
+
+    //TODO : a tester
+    public void removetask(string name) {
+        string filePath = App.Program.projectPath + "\\ListTask\\" + this.name + ".txt";
+        string[] contentFile = File.ReadAllLines(filePath);
+        List<string> contentFileModifed = new List<string>();
+        string[] meta = contentFile[0].Split("~~~");
+        //Modification de la date de derniere modification
+        meta[3] = DateTime.Now.ToString(); 
+        contentFileModifed.Add(string.Join("~~~", meta));
+        for (int i = 0; i < this.tasks.Count; i++)
+        {
+            if(this.tasks[i].name == name) {
+                this.tasks.Remove(this.tasks[i]);
+                this.dtLastModif = DateTime.Now;
+            } else {
+                contentFileModifed.Add(contentFile[i + 1]);
+            }       
+        }
+        File.WriteAllLines(filePath, contentFileModifed);
+    }
+
+    public static ListeTask convertToListTask(string nomListe) {
+        string path = App.Program.projectPath + "\\ListTask\\" + nomListe + ".txt";
+        string[] lines = File.ReadAllLines(path);
+        string[] metaList = lines[0].Split("~~~");
+        ListeTask lt = new ListeTask(metaList[0], DateTime.Parse(metaList[2]));
+        lt.dtLastModif = DateTime.Parse(metaList[3]);
+        lt.id = metaList[1];
+        for(int i = 1; i < lines.Length; i++) {
+            string[] metaTask = lines[i].Split("~~~");
+            Task t = new Task(metaTask[1], metaTask[5], DateTime.Parse(metaTask[3]));
+            t.id = metaTask[2];
+            t.dLastModif = DateTime.Parse(metaTask[4]);
+            t.done = metaTask[0] != "#";
+            lt.tasks.Add(t);
+        }
+        return lt;
+    }   
 }
 
 public class Task
--- utilTask.cs
+++ utilTask.cs
@@ -1,3 +1,6 @@
+using App;
+using ListeTache;
+
 namespace UtilTask
 {
     public static class Utils {
@@ -15,10 +18,17 @@ ____  _____  _____  __ ___   __  __  _____  _____  _____  _____  _____  _____
  |__| \__|__/<_____/|__|__\  \__ \__/\__|__/\__|__/\__|__/\_____/\_____/\__|\_/
                                                                                
         ";
+
         static string instructions = @"
+- select nom_de_la_liste_de_tache : ouvrir une liste de tache 
 - stop : arreter le programme
+- taskList -dt nom_de_la_tache : suprimer une tache de la liste actuelle
+- taskList -d nom_de_la_liste : suprimer une liste
 - taskList -c nom_liste_de_tache : créer une liste de tache
+- taskList -a : afficher toutes les listes de taches 
 - info : renvoie des information sur l'état et la liste de tâche actuelle
+- display : affiche les tasks de la liste de tâche ouverte
+- addTask nom_de_la_tache###description_de_la_tache : ajoute une tache a la liste des taches actuelle 
 
 ";
         public static void startDisplay() {
@@ -42,8 +52,21 @@ ____  _____  _____  __ ___   __  __  _____  _____  _____  _____  _____  _____
                     switch (arr[1])
                     {
                         case "-c":
-                            state.currentLt = ListeTache.ListeTask.creatListTaskEmpty(arr[2]);
+                            state.currentLt = ListeTache.ListeTask.creatListTaskEmpty(convertRegularString(arr[2]));
                             Console.WriteLine("Liste de tache " + state.currentLt.name + " ouverte"); 
+                            break;
+                        case "-a":
+                            Console.WriteLine(displayAll());
+                            break;
+                        case "-dt":
+                            if(state.currentLt == null ) {
+                                Console.WriteLine("Aucune liste de tache ouverte");
+                            } else {
+                                state.currentLt.removetask(arr[2]);
+                            }
+                            break;
+                        case "-d":
+                            deleteFile(arr[2]);
                             break;
                         default:
                             Console.WriteLine("Commande non reconnue " + arr[1]);
@@ -52,11 +75,98 @@ ____  _____  _____  __ ___   __  __  _____  _____  _____  _____  _____  _____
                 } else if(arr[0] == "info") {
                     string info = (state.currentLt == null) ? "aucune liste de tache ouverte" : "Liste de tache actuelle : " + state.currentLt.name;
                     Console.WriteLine(info);
+                }else if(arr[0] == "display"){
+                    displayCurrent(state);
+                }else if(arr[0] == "select"){
+                    select(arr[1], state);
+                }else if(arr[0] == "addTask"){
+                    if(state.currentLt == null) {
+                        Console.WriteLine("aucune liste de tache ouverte");
+                    } else{
+                        string[] nameAndDescr = arr[1].Split("###");
+                        ListeTache.Task t = new ListeTache.Task(convertRegularString(nameAndDescr[0]),convertRegularString(nameAndDescr[1]), DateTime.Now);
+                        state.currentLt.addTask(t);
+                    }
                 } else {
                     Console.WriteLine("Commande non reconnue : " + arr[0]);
                 }
             }
             
+        }
+
+        private static bool deleteFile(string name)
+        {
+            string filePath = App.Program.projectPath + "\\ListTask\\" + name + ".txt";
+            if(!File.Exists(filePath)) {
+                Console.WriteLine("Le fichier n'existe pas");
+                return false;
+            }
+            try
+            {
+                File.Delete(filePath);
+            }
+            catch (System.Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            Console.WriteLine("Fichier suprimé");
+            return true;
+        }
+
+        private static string displayAll()
+        {
+            string dirPath = App.Program.projectPath + "\\ListTask\\";
+            string[] ListTasks = Directory.GetFiles(dirPath, "*.txt", SearchOption.AllDirectories);
+            string res = "";
+            foreach (string List in ListTasks)
+            {
+                string fileName = Path.GetFileName(List);
+                res += fileName.Substring(0, fileName.Length - 4) + "\n";
+            }
+            return res;
+        }
+
+        private static App.State select(string name, App.State state)
+        {
+            name = convertRegularString(name);
+            string filePath = App.Program.projectPath + "\\ListTask\\" + name + ".txt";
+            if (!File.Exists(filePath)){
+                Console.WriteLine("Fichier non trouvé : " + name + ".txt");
+                return state;
+            }
+            state.currentLt = ListeTask.convertToListTask(name);
+            Console.WriteLine("Liste de tache selectionnée : " + name);
+            return state;
+        }
+
+        private static string convertRegularString(string s) {
+            string[] arr = s.Split("_");
+            return string.Join(" ", arr);
+        }
+
+        private static void displayCurrent(State state)
+        {
+            if(state.currentLt == null) {
+                Console.WriteLine("aucune tâche à afficher");
+                return;
+            }
+            List<ListeTache.Task> tasks = state.currentLt.tasks;
+            Console.WriteLine(@"
+################## " + state.currentLt.name + @" ##################");
+            Console.WriteLine(@$"Date de création de la liste : {state.currentLt.dtCreat}
+Date de dernière modification de la liste : {state.currentLt.dtLastModif}
+id de la liste : {state.currentLt.id}");
+            foreach (ListeTache.Task t in tasks)
+            {
+                string check = t.done ? "[X]" : "[]";
+                Console.WriteLine($@"
+{check} {t.name}
+        Créer le {t.dCreat} || Dernière modification {t.dLastModif}
+        id : {t.id}
+        Description : {t.descr}
+                ");
+            }
         }
     }
 }
